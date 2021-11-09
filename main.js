adabd3f796e714b30658ed339d76856b0b0d6b43
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-10 00:43:27 +0100

    display 10 list items (same mail)

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const app = new Vue({
     el: '#app',
     data: {
-
+        mailArr: [],
     },
-    
+
+    mounted(){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response =>{
+            // console.log(response);
+            // console.log(response.data.response);
+            while (app.mailArr.length < 10) {
+                app.mailArr.push(response.data.response);
+                
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
 })
