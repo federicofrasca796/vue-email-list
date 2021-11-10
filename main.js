c3433bc1e0fd0ff543add6d68dfc7a7236a6ef8e
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-10 01:21:01 +0100

    complete bonus: print pre-loaded mails array

--- main.js
+++ main.js
@@ -4,22 +4,39 @@ const app = new Vue({
         mailArr: [],
     },
     methods: {
-        getMail(){
+        pushMail(){
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response =>{
-                    app.mailArr.push(response.data.response);
+                app.mailArr.push(response.data.response);
             })
             .catch(error => {
                 console.error(error);
             })
         },
+
+        //bonus
+        storeMail(){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            then(response =>{
+                const stanbyArr = [];
+                while (standbyArr.length < 10) {
+                    stanbyArr.push(response.data.response);
+                }
+                return standbyArr;
+            })
+        }
     },
 
     mounted(){
         for (let i = 0; i < 10; i++) {
-            this.getMail();
+            this.pushMail();
         }
+
+        //bonus 
+        const readyToPrint = storeMail();
+
     }
 
 })
